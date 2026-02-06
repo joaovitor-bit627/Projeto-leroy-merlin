f738cb87ba002e0c21b257ac61a03166b4c88435
João <omaldia470@gmail.com>
2026-02-06 08:52:45 -0300

    mudança nos textos

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-const photos = document.querySelectorAll('.photo');
-const section = document.querySelector('.sticky-section');
+const photos = document.querySelectorAll(".photo");
+const section = document.querySelector(".sticky-section");
 
-window.addEventListener('scroll', () => {
-  const start = section.offsetTop;
-  const end = start + section.offsetHeight - window.innerHeight;
-  const scroll = window.scrollY;
+window.addEventListener("scroll", () => {
+  const rect = section.getBoundingClientRect();
+  const scrollProgress =
+    (window.innerHeight - rect.top) /
+    (rect.height - window.innerHeight);
 
-  const progress = Math.min(
-    Math.max((scroll - start) / (end - start), 0),
-    1
+  const progress = Math.min(Math.max(scrollProgress, 0), 1);
+
+  const index = Math.min(
+    photos.length - 1,
+    Math.floor(progress * photos.length)
   );
 
-  const index = Math.floor(progress * photos.length);
-
-  photos.forEach((photo, i) => {
-    photo.classList.toggle('active', i === index);
+  photos.forEach((img, i) => {
+    img.classList.toggle("active", i === index);
   });
 });
+
 
 
 const backIntro = document.getElementById("backIntro");
